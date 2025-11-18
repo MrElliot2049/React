ac178d399e09773594e3e6da07d0bdde6a568c15
Adamya Chauhan <adamya1332004@gmail.com>
2025-11-18 21:50:57 +0530

    new

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+import Profile from './components/Profile'
+import Login from './components/login'
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>My context</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
